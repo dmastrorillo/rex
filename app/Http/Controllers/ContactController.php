00ea2c845f7662bc0b898bca76561ac4b2394e16
Daniel Mastrorillo <danielmastrorillo@gmail.com>
2025-03-18 15:14:57 +1000

    create placeholder contacts frontend page and fix login flow. Remove old dashboard initial page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ContactController extends Controller
 {
-    //
+
+
+    public function show()
+    {
+        return Inertia::render('contacts/index');
+    }
 }

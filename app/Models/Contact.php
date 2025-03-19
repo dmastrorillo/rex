98d3e68b7a6022bbf72823fa753d5a26d5232cb1
Daniel Mastrorillo <danielmastrorillo@gmail.com>
2025-03-19 22:25:01 +1000

    add support for API

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = ['firstName', 'surname', 'email', 'phone'];
 }
